4a3996107b6deb9c6e478f4aa9fb440388855edc
Javid <dev22843@gmail.com>
2025-03-24 04:27:36 +0400

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
     import Card from "$lib/components/Card.svelte";
+    import { base } from '$app/paths';
 
     const cards = [
-        { imageSrc: "/img/feign_header.jpeg", title: "Feign", description: "Beautiful nature view" },
-        { imageSrc: "/img/feather_header.jpeg", title: "Feather Party", description: "Urban cityscape" },
-        { imageSrc: "/img/throwia_header.jpeg", title: "Throwia", description: "Majestic mountains" },
-        { imageSrc: "/img/tra_header.jpeg", title: "Trakonius", description: "Deep blue ocean" },
-        { imageSrc: "/img/hoop_header.jpeg", title: "Hoop Fighters", description: "Deep blue ocean" },
-        { imageSrc: "/img/ocean_header.jpeg", title: "Ocean Gap", description: "Deep blue ocean" },
-        { imageSrc: "/img/surviwall_header.jpeg", title: "Surviwall", description: "Deep blue ocean" },
+        { imageSrc: base + "/img/feign_header.jpeg", title: "Feign", description: "Beautiful nature view" },
+        { imageSrc: base + "/img/feather_header.jpeg", title: "Feather Party", description: "Urban cityscape" },
+        { imageSrc: base + "/img/throwia_header.jpeg", title: "Throwia", description: "Majestic mountains" },
+        { imageSrc: base + "/img/tra_header.jpeg", title: "Trakonius", description: "Deep blue ocean" },
+        { imageSrc: base + "/img/hoop_header.jpeg", title: "Hoop Fighters", description: "Deep blue ocean" },
+        { imageSrc: base + "/img/ocean_header.jpeg", title: "Ocean Gap", description: "Deep blue ocean" },
+        { imageSrc: base + "/img/surviwall_header.jpeg", title: "Surviwall", description: "Deep blue ocean" },
     ];
 </script>
 
