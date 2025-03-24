c2298a8320ab35b80acd3c051e851aabffe9e332
Javid <dev22843@gmail.com>
2025-03-24 16:53:27 +0400

    json

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,10 @@
+import { base } from '$app/paths';
+
 export const ssr = false;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-    const res = await fetch('/games.json');
+    const res = await fetch(base + '/games.json');
     const games = await res.json();
 
     return { games };
